e50292651f15851c0acf1a3e0f55bf0eb76da0bc
Pierrick CAEN <pierrick@drivy.com>
2013-06-19 11:44:52 +0200

    Update Twitter plugin

--- js/script.js
+++ js/script.js
@@ -2,19 +2,21 @@
 
 */
 $(document).ready(function() {
-	$("#tweeter_feed").tweetable({
-		username: 'prcaen',
-		time: true,
-		rotate: false,
-		speed: false, 
-		limit: 5, 
-		replies: true, 
-		position: 'append', 
-		onComplete:function($ul){
-			$ul.addClass('clearfix');
-			$ul.children('li').each(function(index) {
-				$(this).prepend('<i class="sprite" id="face">My face</i>');
-			});
-		}
-	});
+  $("#tweeter_feed").tweetable({
+    html5: true,
+    username: 'prcaen',
+    time: true,
+    rotate: false,
+    speed: false,
+    limit: 5,
+    replies: true,
+    position: 'append',
+    failed: "An error has occured with Twitter API",
+    onComplete:function($ul){
+      $ul.addClass('clearfix');
+      $ul.children('li').each(function(index) {
+        $(this).prepend('<i class="sprite" id="face">My face</i>');
+      });
+    }
+  });
 });
